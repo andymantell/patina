001735c755c47f0c6d4aa17e5e1131a3ce8b2325
Andy Mantell <contact@andymantell.com>
2015-11-06 17:49:20 +0000

    Quick and dirty commit of some handlebars rendering

--- app/app.js
+++ app/app.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var app = express();
+var handlebars = require('handlebars');
+var glob = require("glob");
+var path = require('path');
+var fs = require('fs');
 
-app.get('/', function(req, res){
-  res.send('hello world');
+var hbs = handlebars.create();
+
+// Register components
+glob("test/fixtures/example-site/src/components/**/component.yaml", function (er, files) {
+  files.forEach(function(filename) {
+    var name = path.dirname(path.relative('test/fixtures/example-site/src/components', filename));
+    hbs.registerPartial(name, fs.readFileSync(path.join(path.dirname(filename), 'template.hbs'), 'utf8'));
+  });
+});
+
+// Register layouts
+glob("test/fixtures/example-site/src/layouts/**/*.hbs", function (er, files) {
+  files.forEach(function(filename) {
+    hbs.registerPartial('layout-' + path.basename(filename, path.extname(filename)), fs.readFileSync(filename, 'utf8'));
+  });
+});
+
+app.get('/', function(req, res) {
+  res.redirect('/components/page-index');
+});
+
+app.get('/components/:component', function(req, res){
+  res.send(hbs.compile(hbs.partials['layout-main'])({
+    body: handlebars.compile(hbs.partials[req.params.component])
+  }));
 });
 
 app.listen(3000);
