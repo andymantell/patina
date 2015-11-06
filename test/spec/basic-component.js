96241899d8a12306394e41a27a54a4ce8d68b2e4
Andy Mantell <contact@andymantell.com>
2015-11-06 16:10:59 +0000

    Write failing tests for a basic component

--- test/spec/basic-component.js
+++ test/spec/basic-component.js
@@ -1,1 +1,24 @@
 var should = require('should');
+var request = require('supertest')
+
+var app = require('../../app/app');
+
+describe('A basic component', function() {
+
+  it('should have a demo rendered at /components/basic-component', function(done) {
+    request(app)
+      .get('/basic-component')
+      .expect('Content-Type', /text\/html/)
+      .expect(200, done);
+  });
+
+  it('should be listed on the index page', function(done) {
+    request(app)
+      .get('/')
+      .expect(function(res) {
+        res.text.should.containEql('<a href="/components/basic-component">A basic component</a>');
+      })
+      .end(done);
+  });
+
+});
