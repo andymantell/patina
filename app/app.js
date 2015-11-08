911dac791dfef47978e1ca7a380b6cf2a2f8b57c
Andy Mantell <contact@andymantell.com>
2015-11-08 15:41:13 +0000

    Initial work on frontmatter for demos

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var handlebars = require('handlebars');
 var glob = require("glob");
 var path = require('path');
 var fs = require('fs');
+var yfm = require('yfm');
 
 var hbs = handlebars.create();
 
@@ -15,23 +16,30 @@ glob("test/fixtures/example-site/src/components/**/component.yaml", function (er
   files.forEach(function(filename) {
     var name = path.dirname(path.relative('test/fixtures/example-site/src/components', filename));
 
-    components.push({
-      name: name, // @TODO: Pull name from component.yaml
-      href: '/components/' + name
-    });
-
     hbs.registerPartial(name, fs.readFileSync(path.join(path.dirname(filename), 'template.hbs'), 'utf8'));
+
+    // Register component demo variants
+    glob(path.join(path.dirname(filename), 'demos/*.hbs'), function(er, files) {
+      files.forEach(function(demoFilename) {
+        components.push({
+          name: name + ': ' + path.basename(demoFilename, path.extname(demoFilename)), // @TODO: Pull name from component.yaml
+          href: '/components/' + name + '/' + path.basename(demoFilename, path.extname(demoFilename))
+        });
+
+        // hbs.registerPartial('demos/' + name + '/' + path.basename(demoFilename, path.extname(demoFilename)), fs.readFileSync(demoFilename, 'utf8'));
+      });
+    });
   });
 });
 
 // Register pages
 glob("test/fixtures/example-site/src/pages/**/component.yaml", function (er, files) {
   files.forEach(function(filename) {
-    var name = path.dirname(path.relative('test/fixtures/example-site/src/pages', filename));
+    var name = 'page/' + path.dirname(path.relative('test/fixtures/example-site/src/pages', filename));
 
     pages.push({
       name: name, // @TODO: Pull name from component.yaml
-      href: '/page/' + name
+      href: '/' + name
     });
 
     hbs.registerPartial(name, fs.readFileSync(path.join(path.dirname(filename), 'template.hbs'), 'utf8'));
@@ -52,9 +60,13 @@ app.get('/', function(req, res) {
   }));
 });
 
-app.get('/components/:component', function(req, res){
+app.get('/components/:component/:variant', function(req, res){
+
+  var page = fs.readFileSync(path.join('test/fixtures/example-site/src/components', req.params.component,  'demos', req.params.variant + '.hbs'), 'utf8');
+  var pageData = yfm(page);
+
   res.send(hbs.compile(hbs.partials['layout/main'])({
-    body: hbs.compile(hbs.partials[req.params.component])
+    body: hbs.compile(pageData.content)(pageData.context)
   }));
 });
 
--- test/spec/basic-component.js
+++ test/spec/basic-component.js
@@ -5,18 +5,31 @@ var app = require('../../app/app');
 
 describe('A basic component', function() {
 
-  it('should have a demo rendered at /components/basic-component', function(done) {
+  it('should have a demo rendered at /components/basic-component/basic-demo', function(done) {
     request(app)
-      .get('/components/basic-component')
+      .get('/components/basic-component/basic-demo')
       .expect('Content-Type', /text\/html/)
-      .expect(200, done);
+      .expect(200)
+      .expect(function(res) {
+        res.text.should.containEql('This is a basic component demo');
+      })
+      .end(done);
   });
 
   it('should be listed on the index page', function(done) {
     request(app)
       .get('/')
       .expect(function(res) {
-        res.text.should.containEql('<a href="/components/basic-component">basic-component</a>');
+        res.text.should.containEql('<a href="/components/basic-component/basic-demo">basic-component: basic-demo</a>');
+      })
+      .end(done);
+  });
+
+  it('should have documentation rendered on the component page', function(done) {
+    request(app)
+      .get('/')
+      .expect(function(res) {
+        res.text.should.containEql('<a href="/components/basic-component/basic-demo">basic-component: basic-demo</a>');
       })
       .end(done);
   });
